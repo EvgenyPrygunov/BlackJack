22a0ea89bc1105891703b72d4726f63efe29f528
Evgeny Prygunov <prygunov.ea@gmail.com>
2018-03-10 16:41:22 +0300

    Adding start method

--- logic.rb
+++ logic.rb
@@ -1,1 +1,23 @@
 #logic
+class Logic
+  def initialize
+    @player = ''
+    @dealer = Dealer.new
+    @deck = Deck.new
+    #@bank = Bank.new
+  end
+
+  def start
+    puts 'Hello! What is your name?'
+    name = gets.chomp.to_s
+    @player = Player.new(name)
+    @player.cards = @deck.get_cards(2)
+    @dealer.cards = @deck.get_cards(2)
+    puts 'Your cards:'
+    @player.cards.each { |card| puts "#{card[0]}#{card[1]}" }
+    puts 'Dealer cards:'
+    @player.cards.each { |card| puts "*" }
+  end
+
+
+end
